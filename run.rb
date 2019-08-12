dd11c5feff62b6fde2859eb29d5024f490d6d9fb
Max Sukhanov <fubar00@yandex.ru>
2019-08-12 15:28:29 +0300

    add files with user interface and enter point + add in main files user interaction logic

---
+++ run.rb
@@ -0,0 +1,3 @@
+require_relative "usr_interaction"
+
+DirectoryCheckerApp.run
---
+++ usr_interaction.rb
@@ -0,0 +1,32 @@
+require_relative 'list_of_directory_file_names'
+require_relative 'count_same_files_report'
+
+class DirectoryCheckerApp
+  def self.run
+    DirectoryCheckerApp.new.run
+  end
+
+  def run
+    puts "Hi there. Would you like to get the list of analysed directory files? (y or any key to skip)"
+    lst = gets.chomp
+      if lst == "y"
+        files = ListOfDirectoryFileNames.new
+        list = files.files_list
+        puts "Here is the list of analyzed directory files:"
+        puts list
+      else
+        puts "Okay"
+      end
+
+    puts "Would you like to find files with the same content? (y or any key to skip)"
+    rep = gets.chomp
+      if rep == "y"
+        count = CountSameFilesReport.new
+        report = count.frequency_report_of_equal_files
+        puts "Name of the analyzed directory: "
+        puts report
+      else
+        puts "Okay, bye"
+      end
+  end 
+end
